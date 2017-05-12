c33b2a2cd048be5bbf7009a04cccd7817797a74f
GonkDroid <osborne.byron@gmail.com>
2017-05-12 08:27:53 -0700

    Initial Commit

---
+++ The Shards of the Infinite/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  The Shards of the Infinite
+//
+//  Created by Mobile on 5/12/17.
+//  Copyright © 2017 SBHS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
