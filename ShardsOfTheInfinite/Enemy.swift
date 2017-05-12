4ecf721c12bf41271059960f6ac644be52b89c66
Mobile <mobile@rm26imac018.local>
2017-05-12 08:54:49 -0700

    Weapon & WeaponDictionary Classes Completed Enemy Class Completed

--- ShardsOfTheInfinite/Enemy.swift
+++ ShardsOfTheInfinite/Enemy.swift
@@ -7,3 +7,63 @@
 //
 
 import Foundation
+
+class Enemy{
+    var name:String! = ""
+    var experienceWorth:Int! = 0
+    var health:Int! = 0
+    var evasion:Int! = 0
+    var defense:Int! = 0
+    var vis:Int! = 0
+    var magicAttack:Int! = 0
+    var coins:Int! = 0
+    var lootLevel:Int! = 0
+    
+    init(name: String, experienceWorth: Int, health: Int, evasion: Int, defense: Int, vis: Int, magicAttack: Int, coins: Int, lootLevel: Int){
+        self.name = name
+        self.experienceWorth = experienceWorth
+        self.health = health
+        self.evasion = evasion
+        self.defense = defense
+        self.vis = vis
+        self.magicAttack = magicAttack
+        self.coins = coins
+        self.lootLevel = lootLevel
+    }
+    
+    func enemyName() -> String{
+        return name
+    }
+    
+    func enemyExperienceWorth() -> Int{
+        return experienceWorth
+    }
+    
+    func enemyHealth() -> Int{
+        return health
+    }
+    
+    func enemyEvasion() -> Int{
+        return evasion
+    }
+    
+    func enemyDefense() -> Int{
+        return defense
+    }
+    
+    func enemyVis() -> Int{
+        return vis
+    }
+    
+    func enemyMagicAttack() -> Int{
+        return magicAttack
+    }
+    
+    func enemyCoins() -> Int{
+        return coins
+    }
+    
+    func enemyLootLevel() -> Int{
+        return lootLevel
+    }
+}
--- ShardsOfTheInfinite/Weapon.swift
+++ ShardsOfTheInfinite/Weapon.swift
@@ -7,3 +7,32 @@
 //
 
 import Foundation
+
+class Weapon{
+    var damage:Int! = 0
+    var hitChance:Int! = 0
+    var blockingDefense:Int! = 0
+    var name:String! = ""
+    
+    init(name: String, damage: Int, hitChance: Int, blockingDefense: Int) {
+        self.damage = damage
+        self.hitChance = hitChance
+        self.blockingDefense = blockingDefense
+        self.name = name
+    }
+    
+    func weaponDamage() -> Int{
+        return damage
+    }
+    
+    func weaponHitChance() -> Int{
+        return hitChance
+    }
+    
+    func weaponBlockingDefense() -> Int{
+        return blockingDefense
+    }
+    func weaponName() -> String{
+        return name
+    }
+}
