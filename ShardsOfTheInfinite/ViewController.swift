2e458eb9d1f9e7b114305931df62c22bbda7b80d
Mobile <mobile@rm26imac018.local>
2017-05-12 08:56:53 -0700

    from kenny,

--- ShardsOfTheInfinite/ViewController.swift
+++ ShardsOfTheInfinite/ViewController.swift
@@ -13,7 +13,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        //hey a change
+        //hey a change blabalblbalalbal
     }
 
     override func didReceiveMemoryWarning() {
