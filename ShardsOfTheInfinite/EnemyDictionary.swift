b9539b1425e21b19ba2a4206cd2448c49bf0a13d
Mobile <mobile@rm26imac018.local>
2017-05-12 08:29:57 -0700

    test

---
+++ ShardsOfTheInfinite/EnemyDictionary.swift
@@ -0,0 +1,9 @@
+//
+//  EnemyDictionary.swift
+//  ShardsOfTheInfinite
+//
+//  Created by Mobile on 5/12/17.
+//  Copyright © 2017 SBHS. All rights reserved.
+//
+
+import Foundation
---
+++ ShardsOfTheInfinite/WeaponDictionary.swift
@@ -0,0 +1,9 @@
+//
+//  WeaponDictionary.swift
+//  ShardsOfTheInfinite
+//
+//  Created by Mobile on 5/12/17.
+//  Copyright © 2017 SBHS. All rights reserved.
+//
+
+import Foundation
